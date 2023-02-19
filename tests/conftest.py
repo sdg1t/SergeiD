7035fe80cf7735378119bef0c5e676541e6b8595
Sergei Dubrov <sdubrov@icloud.com>
2023-02-19 12:30:22 +0000

    Added one more branch changes

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+class User:
+
+    def __init__(self) -> None:
+        self.name = None 
+        self.second_name = None
+
+    def create(self):
+        self.name = 'Sergei'
+        self.second_name = 'Dubrov'
+
+    def remove(self):
+        self.name = ''
+        self.second_name = ''
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+
+    yield user 
+
+    user.remove()
